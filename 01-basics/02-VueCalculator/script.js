d39df57587b372978f4bb10b21beededc68b1c9d
Alexander Krymov <alexanderkrymov@ya.ru>
2021-11-21 20:57:00 +0300

    Homework number one

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+const App = {
+  data() {
+    return {
+      a: null,
+      b: null,
+      operator: 'sum',
+    };
+  },
+  computed: {
+    result() {
+      switch (this.operator) {
+        case 'sum':
+          return this.a + this.b;
+        case 'subtract':
+          return this.a - this.b;
+        case 'multiply':
+          return this.a * this.b;
+        case 'divide':
+          return this.a / this.b;
+      }
+    },
+  },
+};
+
+createApp(App).mount('#app');
